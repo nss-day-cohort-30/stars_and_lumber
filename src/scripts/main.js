3a1046516222871ae62dc7f8106f2cefc9b9a40f
Steve Brownlee <chortlehoort@gmail.com>
2019-02-12 11:04:20 -0600

    Master version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,5 @@
 import APIManager from "./APIManager"
-import createEmployee from "./createEmployee"
-import printToDOM from "./printToDOM"
 
 APIManager.getEmployees().then(
-    (employees) => {
-        employees.forEach(
-            employee => {
-                const employeeHTML = createEmployee(employee)
-                printToDOM(employeeHTML, ".employeeList")
-            }
-        )
-    }
+    employees => { }
 )
--- src/scripts/printToDOM.js
+++
@@ -1,5 +0,0 @@
-const printToDOM = (what, where) => {
-    document.querySelector(where).innerHTML += what
-}
-
-export default printToDOM
